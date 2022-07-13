1cb229b09a2788685f7d41ea70babc5f3480f5ef
Olga Murina <olgamurina92@gmail.com>
2022-07-13 23:30:02 +0300

    add register page

---
+++ src/components/ValidationErrors.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul class="error-messages">
+        <li v-for="errorMessage in errorMessages" :key="errorMessage">
+            {{ errorMessage }}
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'McvValidationErrors',
+    props: {
+        validationErrors: {
+            type: Object,
+            required: true,
+        },
+    },
+    computed: {
+        errorMessages() {
+            return Object.keys(this.validationErrors).map(name => {
+                return this.validationErrors[name].join(', ');
+            });
+        },
+    },
+};
+</script>
