106f991f425211ed9a44f08eede819f42c193c9e
Olga Murina <olgamurina92@gmail.com>
2022-07-16 13:16:43 +0300

    create login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import {createRouter, createWebHashHistory} from 'vue-router';
 import HomeView from '@/views/HomeView';
 import Register from '@/views/Register';
+import Login from '@/views/Login';
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/register',
-    name: 'register',
-    component: Register
-  },
-]
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView,
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: Register,
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login,
+    },
+];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
+    history: createWebHashHistory(),
+    routes,
+});
 
-export default router
+export default router;
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,23 +3,14 @@
         <div class="container page">
             <div class="row">
                 <div class="col-md-6 offset-md-3 col-xs-12">
-                    <h1 class="text-xs-center">Sign Up</h1>
+                    <h1 class="text-xs-center">Sign In</h1>
                     <p class="text-xs-center">
-                        <router-link :to="{name: 'login'}">Have an account?</router-link>
+                        <router-link :to="{name: 'register'}">Need an account?</router-link>
                     </p>
                     <mcv-validation-errors 
                         v-if="validationErrors"
                         :validation-errors="validationErrors"/>
                     <form @submit.prevent="onSubmit">
-                        <fieldset class="form-group">
-                            <input
-                                type="text"
-                                class="form-control form-control-lg"
-                                placeholder="Username"
-                                v-model="username"
-                            />
-                        </fieldset>
-
                         <fieldset class="form-group">
                             <input
                                 type="email"
@@ -37,7 +28,7 @@
                                 v-model="password"
                             />
                         </fieldset>
-                        <button class="btn btn-lg btn-primary pull-xs-right" :disabled="isSubmitting">Sign Up</button>
+                        <button class="btn btn-lg btn-primary pull-xs-right" :disabled="isSubmitting">Sign In</button>
                     </form>
                 </div>
             </div>
@@ -46,35 +37,33 @@
 </template>
 
 <script>
+import {mapState} from 'vuex';
+
 import McvValidationErrors from '@/components/ValidationErrors';
 import {actionTypes} from '@/store/modules/auth';
 
 export default {
-    name: 'McvRegister',
-    components: {
-        McvValidationErrors,
-    },
+    name: 'McvLogin',
     data() {
         return {
             email: '',
             password: '',
-            username: '',
         };
     },
+    components: {
+        McvValidationErrors,
+    },
     computed: {
-        isSubmitting() {
-            return this.$store.state.auth.isSubmitting;
-        },
-        validationErrors() {
-            return this.$store.state.auth.validationErrors;
-        }
+        ...mapState({
+            isSubmitting: state => state.auth.isSubmitting,
+            validationErrors: state => state.auth.validationErrors,
+        })
     },
     methods: {
         onSubmit() {
             this.$store
-                .dispatch(actionTypes.register, {
+                .dispatch(actionTypes.login, {
                     email: this.email,
-                    username: this.username,
                     password: this.password,
                 })
                 .then(() => {
